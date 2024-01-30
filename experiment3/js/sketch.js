c40c65930fd28b20a251ea62470521a4e5afc002
JuiceBox7 <juiceinbox7@gmail.com>
2024-01-29 19:22:46 -0800

    finished Experiment 3

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,6 +1,6 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Julian Lara
+// Date: 1/29/24
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
@@ -14,54 +14,89 @@ const VALUE2 = 2;
 let myInstance;
 let canvasContainer;
 
+let x, y;
+let currAngle = 0;
+const step = 20;
+const angle = 90;
+
+let axiom = 'F';
+let numIterations = 5;
+let rules = [];
+rules[0] = ['F', 'F-F-F-F+F+F-F-F+F'];
+
+let currChar = 0;
+
 class MyClass {
     constructor(param1, param2) {
         this.property1 = param1;
         this.property2 = param2;
     }
-
-    myMethod() {
-        // code to run when method is called
-    }
 }
 
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
     let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
     canvas.parent("canvas-container");
-    // resize canvas is the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+    background(0);
+    x = width/2;
+    y = height/2;
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    for (let i = 0; i < numIterations; i++) {
+    axiom = lindenmayer(axiom);
+    }
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+  drawIt(axiom[currChar]);
+  currChar++;
+  if (currChar > axiom.length-1) currChar = 0;
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function lindenmayer(s) {
+  let output = '';
+
+  for (let i = 0; i < s.length; i++) {
+    let isMatch = 0;
+    for (let j = 0; j < rules.length; j++) {
+      if (s[i] == rules[j][0])  {
+        output += rules[j][1];
+        isMatch = 1;
+        break;
+      }
+    }
+    if (isMatch == 0) output+= s[i];
+  }
+  return output;
+}
+
+function drawIt(k) {
+
+  if (k=='F') {
+    let x1 = x + step*cos(radians(currAngle));
+    let y1 = y + step*sin(radians(currAngle));
+    line(x, y, x1, y1);
+
+    x = x1;
+    y = y1;
+  }
+  if (k == '+') currAngle += angle;
+  if (k == '-') currAngle -= angle;
+
+  let r = random(0, 255);
+  let g = random(0, 255);
+  let b = random(0, 255);
+  let a = random(50, 100);
+
+  let size = 0;
+  size += random(0, 25);
+  size += random(0, 25);
+  size += random(0, 25);
+  size = size / 5;
+
+  fill(r, g, b, a);
+  square(x, y, size);
 }
